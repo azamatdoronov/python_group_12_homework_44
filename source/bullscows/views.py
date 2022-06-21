006e8b05aa07c3289756cf0305f6d9622166c07a
azamatdoronov <aza3el@inbox.ru>
2022-06-21 16:37:29 +0600

    Make def one_check

--- source/bullscows/views.py
+++ source/bullscows/views.py
@@ -7,3 +7,17 @@ class Check:
         self.numbers = None
 
     secret_numbers = [1, 2, 3, 4]
+
+    def one_check(self, numbers_str):
+        try:
+            numbers = [int(s) for s in numbers_str]
+            if len(numbers) != 4:
+                return "You should enter only 4 numbers"
+            if len(numbers) != len(set(numbers)):
+                return "Numbers should be unique"
+            for i in numbers:
+                if i < 0 or i > 10:
+                    return "Numbers should be in range 1 to 9"
+            self.numbers = numbers
+        except:
+            return " The value should be integers"
